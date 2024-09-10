a3e40c50e55fefb033275becf233113a3c8d3136
Viktor Riabkov <vriabkov@scnsoft.com>
2024-09-10 19:10:41 +0200

    Update yarn version and configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import airbnb from 'eslint-config-airbnb';
+
+export default [
+  {
+    ignores: ['dist', 'node_modules', '.yarn', 'public', 'yarn.lock'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': ts,
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...ts.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...airbnb.rules,
+      'prettier/prettier': 'warn', // Prettier as ESLint
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  // Disable ESLint rules that have conflicts with Prettier
+  prettierConfig,
+];
