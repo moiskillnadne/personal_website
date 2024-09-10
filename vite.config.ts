b96790ffe48deb3fb40d9432d6e4ed21c23649fb
Viktor Riabkov <vriabkov@scnsoft.com>
2024-09-10 19:19:17 +0200

    Github action on open merge request

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    // eslint-disable-next-line no-undef
     alias: [{ find: '~', replacement: path.resolve(__dirname, './src') }],
   },
 });
