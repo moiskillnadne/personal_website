1315ddaa92f335f50c5e6cb12c8859179291a31c
Viktor Riabkov <vriabkov@scnsoft.com>
2024-09-11 18:07:59 +0200

    Typography component extended by font-weight

--- src/shared/ui/Typography/index.tsx
+++ src/shared/ui/Typography/index.tsx
@@ -1,11 +1,29 @@
 type Props = {
   value: string;
 
-  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl';
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl';
+  weight?:
+    | 'thin'
+    | 'extralight'
+    | 'light'
+    | 'normal'
+    | 'medium'
+    | 'semibold'
+    | 'bold'
+    | 'extrabold'
+    | 'black';
 };
 
 function Typography(props: Props) {
-  return <p className={`typography text-${props.size}`}>{props.value}</p>;
+  const defaultSize = 'md';
+
+  const defaultWeight = 'normal';
+
+  const size = props.size ?? defaultSize;
+
+  const weight = props.weight ?? defaultWeight;
+
+  return <p className={`typography font-${weight} text-${size}`}>{props.value}</p>;
 }
 
 export default Typography;
