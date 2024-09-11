e1851a0b0aebcf8dd7fbbbcc702b9537016a3bf0
Viktor Riabkov <vriabkov@scnsoft.com>
2024-09-11 11:52:51 +0200

    Support route added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import HomePage from '~/pages/Home';
 import VpnPage from '~/pages/Vpn';
+import { SupportPage } from '~/pages/Support';
 
 import { AppearanceBackground } from './AppearanceBackground';
 
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
     children: [
       { path: ROUTES.HOME.path, element: <HomePage /> },
       { path: ROUTES.VPN.path, element: <VpnPage /> },
+      { path: ROUTES.SUPPORT.path, element: <SupportPage /> },
     ],
   },
 ]);
--- src/shared/constants/ROUTES.ts
+++ src/shared/constants/ROUTES.ts
@@ -11,4 +11,8 @@ export const ROUTES = {
     path: '/vpn',
     name: 'VPN',
   },
+  SUPPORT: {
+    path: '/support',
+    name: 'Support',
+  },
 };
