3c12a025fe88de3f6f89a2cc4a8771322c1c8f25
Victor Ryabkov <45964820+moiskillnadne@users.noreply.github.com>
2024-09-11 12:43:23 +0300

    Feature: App Version (#6)
    * Dotenv and app version
    
    * extract release version
    
    ---------
    
    Co-authored-by: Viktor Riabkov <vriabkov@scnsoft.com>

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../widget/Header';
+import { Header } from '~/widget/Header';
 
 export const Layout = () => {
+  const appVersion = import.meta.env.VITE_APP_VERSION;
+
   return (
     <div id="app-layout">
       <Header />
       <div className="app-content-template">
         <Outlet />
       </div>
+
+      <span style={{ margin: '12px 16px', opacity: '0.35', fontSize: '12px' }}>{appVersion}</span>
     </div>
   );
 };
