b195aa7c992de52f694a94a9d75340625326b88a
Viktor Riabkov <vriabkov@scnsoft.com>
2024-09-12 09:21:52 +0200

    Fixed error with dynamic classNames and tailwind

--- src/shared/ui/Typography/index.tsx
+++ src/shared/ui/Typography/index.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 type Props = {
   value: string;
 
@@ -15,6 +17,31 @@ type Props = {
 };
 
 function Typography(props: Props) {
+  const sizeClassesMap = {
+    xs: 'text-xs',
+    sm: 'text-sm',
+    md: 'text-md',
+    lg: 'text-lg',
+    xl: 'text-xl',
+    '2xl': 'text-2xl',
+    '3xl': 'text-3xl',
+    '4xl': 'text-4xl',
+    '5xl': 'text-5xl',
+    '6xl': 'text-6xl',
+  };
+
+  const weightClassesMap = {
+    thin: 'font-thin',
+    extralight: 'font-extralight',
+    light: 'font-light',
+    normal: 'font-normal',
+    medium: 'font-medium',
+    semibold: 'font-semibold',
+    bold: 'font-bold',
+    extrabold: 'font-extrabold',
+    black: 'font-black',
+  };
+
   const defaultSize = 'md';
 
   const defaultWeight = 'normal';
@@ -23,7 +50,9 @@ function Typography(props: Props) {
 
   const weight = props.weight ?? defaultWeight;
 
-  return <p className={`typography font-${weight} text-${size}`}>{props.value}</p>;
+  const combinedClass = classNames(sizeClassesMap[size], weightClassesMap[weight]);
+
+  return <p className={combinedClass}>{props.value}</p>;
 }
 
 export default Typography;
